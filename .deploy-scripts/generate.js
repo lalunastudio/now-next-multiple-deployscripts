f27bfe4f005529494e55da1ad3e04996a251592d
Peter Nielsen <peter@designmonkey.dk>
2019-02-24 23:00:28 +0100

    include build scripts and add now alias to each deployment script

--- .deploy-scripts/generate.js
+++ .deploy-scripts/generate.js
@@ -12,34 +12,43 @@ const shops = [
 		regions: ['all'],
 	},
 ];
-
 const defaultTemplateData = fs.readFileSync(process.env.PWD + '/now.json');
 const defaultTemplate = JSON.parse(defaultTemplateData);
 const scripts = [];
 const allScripts = [];
 
 shops.forEach(shop => {
-	const shopJson = {
-		...defaultTemplate,
-		name: defaultTemplate.name + '-' + shop.slug,
-		regions: shop.regions,
-		alias: shop.domains,
-		build: {
-			env: {
-				SHOP_SLUG: shop.slug,
-			},
-		},
-	};
+  const shopJson = {
+    ...defaultTemplate,
+    name: defaultTemplate.name + '-' + shop.slug,
+    regions: shop.regions,
+    alias: shop.domains,
+    build: {
+      ...(defaultTemplate.build || {}),
+      env: {
+        ...((defaultTemplate.build && defaultTemplate.build.env) || {}),
+        SHOP_SLUG: shop.slug,
+      },
+    },
+  };
 
-	const shopJsonData = JSON.stringify(shopJson, null, 2);
-	fs.writeFileSync(process.env.PWD + '/.deploy-scripts/now-' + shop.slug + '.json', shopJsonData);
-	console.log('WRITING: now-' + shop.slug + '.json');
-	scripts.push('"deploy-' + shop.slug + '": "now -A ./.deploy-scripts/now-' + shop.slug + '.json"');
-	allScripts.push('npm run deploy-' + shop.slug);
+  const shopJsonData = JSON.stringify(shopJson, null, 2);
+  fs.writeFileSync(process.env.PWD + '/.deploy-scripts/now-' + shop.slug + '.json', shopJsonData);
+  console.log('WRITING: now-' + shop.slug + '.json');
+  scripts.push(
+    '"deploy-' +
+      shop.slug +
+      '": "now -A ./.deploy-scripts/now-' +
+      shop.slug +
+      '.json && now alias -A ./.deploy-scripts/now-' +
+      shop.slug +
+      '.json"',
+  );
+  allScripts.push('npm run deploy-' + shop.slug);
 });
 
 console.log('\n\n##### DEPLOYMENT SCRIPTS ######');
-console.log('\n - Copy this to package.json\n');
+console.log('\n- Copy this to package.json\n');
 
 console.log(scripts.join(',\n') + ',');
 console.log('"deploy-all": "' + allScripts.join(' && ') + '"');
